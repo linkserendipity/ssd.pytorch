af1acbcb1a7afd92bd4a56a983335d2fc308df98
linkserendipity <462710683@qq.com>
2020-07-11 13:42:01 +0800

    modify extras

--- ssd.py
+++ ssd.py
@@ -153,9 +153,12 @@ def add_extras(cfg, i, batch_norm=False):
     flag = False
     for k, v in enumerate(cfg):
         if in_channels != 'S':
-            if v == 'S':
+            if v == 'S' and in_channels != 128:
                 layers += [nn.Conv2d(in_channels, cfg[k + 1],
                            kernel_size=(1, 3)[flag], stride=2, padding=1)]
+            elif v == 'S' and in_channels == 128:
+                layers += [nn.Conv2d(in_channels, cfg[k + 1],
+                           kernel_size=(1, 3)[flag], stride=2, padding=0)]                
             else:
                 layers += [nn.Conv2d(in_channels, v, kernel_size=(1, 3)[flag])]
             flag = not flag
@@ -186,7 +189,8 @@ base = {
     '512': [],
 }
 extras = {
-    '300': [256, 'S', 512, 128, 'S', 256, 128, 256, 128, 256],
+    #'300': [256, 'S', 512, 128, 'S', 256, 128, 256, 128, 256],
+    '300': [256, 'S', 512, 128, 'S', 256],
     '512': [],
 }
 mbox = {
